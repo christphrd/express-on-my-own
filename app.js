5f133e5e9f2f2a87b6f6445f1d122d074c982c07
github username <diep.christopher@gmail.com>
2018-06-04 16:30:24 -0400

    two routes for get

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
-const http = require("http")
 const express = require("express")
 
-const server = http.createServer( (req, res) => {
-  //express for routing
+const app = express();
+
+/*
+app has useful methods like
+app.get() takes two arguments url and cb func
+app.post()
+app.put()
+app.delete()
+*/
+
+app.get( '/', (req, res) => {
+  res.send('Hello World!');
 })
 
-server.listen(3000);
+app.get('/api/numbers', (req, res) => {
+  res.send([1,2,3]);
+})
+
+app.listen(3000, () => console.log('listening on port 3000 using express'))
